67d2ae165175d5d935261f3bd9fb073b820840a4
TheoLeFur <theo.lefur@epfl.ch>
2023-09-17 13:30:22 +0200

    one has to figure out how to sample from boundary

--- sampler/american_call_option_sampler.py
+++ sampler/american_call_option_sampler.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List
+from typing import List, Optional, Callable, Tuple
 
 from sampler.abstract_sampler import AbstractSampler
 
@@ -20,7 +20,7 @@ class AmericanCallOptionSampler(AbstractSampler):
         :param n_dim: Number of dimensions
         :param t_start: Start time
         :param t_end: End time
-        :param domain: Stock price domain, that is assumed to be of the form  [a1, b1] x ... x [an, bn]
+        :param domain: Stock price domain, that is assumed to be of the form  [a, b] x ... x [a, b]
         TODO : Implement a class domain for more complex domains
         """
         super().__init__(
@@ -30,30 +30,39 @@ class AmericanCallOptionSampler(AbstractSampler):
 
         self.t_start = t_start
         self.t_end = t_end
-
-        if len(domain) != self.n_dim:
-            raise ValueError("Dimension of the domain and n_dim do not match")
-        for _, interval in domain:
-            if interval[0] >= interval[1]:
-                raise ValueError("Lower bound of interval should be strictly smaller than upper bound")
-
         self.domain = domain
 
-    def sample_domain(self, probability_law=None) -> np.ndarray:
+    def sample_domain(self, probability_law: Optional[Callable] = None) -> np.ndarray:
+        """
+        Sample points from the interior of the domain
+        :param probability_law: Probability Law we wish to sample from
+        :return: Output array
+        """
         if probability_law is None:
-            probability_law = np.random.uniform
+            probability_law: Callable = np.random.uniform
             sampled_points: np.ndarray = np.concatenate(
                 [probability_law(self.t_start, self.t_end, (self.n_points, 1)),
-                 *[probability_law(
-                     self.domain[i][0], self.domain[i][1], (self.n_points, 1)) for i, _ in enumerate(self.domain)]
+                 probability_law(
+                     self.domain[0], self.domain[1], (self.n_points, self.n_dim))
                  ]
             )
+
             return sampled_points
         else:
             raise NotImplementedError
 
-    def sample_boundary(self, probability_law=None) -> np.ndarray:
-        pass
+    def sample_boundary(self, probability_law: Optional[Callable] = None) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Sample points from the interior of the domain
+        :param probability_law: Probability Law we wish to sample from
+        :return: Output array
+        """
 
-    def sample_initial(self, probability_law=None) -> np.ndarray:
+        if probability_law is None:
+            probability_law = np.random.uniform
+            sampled_points: np.ndarray = np.concatenate(
+                [probability_law(self)]
+            )
+
+    def sample_initial(self, probability_law: Optional[Callable] = None) -> np.ndarray:
         pass
