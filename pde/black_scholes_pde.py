557d5c0805e60ad02a1211f3262f628188b78843
TheoLeFur <theo.lefur@epfl.ch>
2023-09-19 15:31:25 +0200

    Added parabolic PDE

---
+++ pde/black_scholes_pde.py
@@ -0,0 +1,28 @@
+from pde.parabolic_pde import ParabolicPDE
+import numpy as np
+
+
+class BlackScholesPDE(ParabolicPDE):
+
+    def __init__(
+            self,
+            drift: np.ndarray,
+            diffusion_matrix: np.ndarray,
+            rate: float,
+            name: str,
+            time_dimension: bool = False,
+            space_dimension: int = 1,
+    ):
+
+        super().__init__(
+            drift=drift,
+            diffusion_matrix=diffusion_matrix,
+            name=name,
+            time_dimension=time_dimension,
+            space_dimension=space_dimension
+        )
+
+        if rate < 0:
+            raise ValueError("Negative rates not allowed")
+        else:
+            self.rate = rate
--- pde/parabolic_pde.py
+++ pde/parabolic_pde.py
@@ -1,9 +1,32 @@
+import numpy as np
+
 from pde.abstract_pde import AbstractPDE
+
 
 class ParabolicPDE(AbstractPDE):
 
     def __init__(
             self,
-            name,
-
+            drift: np.ndarray,
+            diffusion_matrix: np.ndarray,
+            name: str,
+            time_dimension: bool = False,
+            space_dimension: int = 1,
     ):
+        super().__init__(
+            name=name,
+            time_dimension=time_dimension,
+            space_dimension=space_dimension,
+        )
+
+        if np.shape(drift) == (self.space_dimension, 1):
+            self.drift = drift
+        else:
+            raise ValueError("Drift vector is not consistent with specified dimension")
+
+        if np.shape(diffusion_matrix) == (self.space_dimension, self.space_dimension):
+            self.diffusion_matrix = diffusion_matrix
+        else:
+            raise ValueError("Diffusion matrix is not consistent with specified dimension")
+
+
