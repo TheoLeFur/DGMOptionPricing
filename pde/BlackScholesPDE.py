4a2192f25e5baf90a8d8e461d085a5d4394fbabc
TheoLeFur <theo.lefur@epfl.ch>
2023-12-02 18:54:52 +0100

    feat: utils for second order evals

--- pde/BlackScholesPDE.py
+++ pde/BlackScholesPDE.py
@@ -3,6 +3,9 @@ import numpy as np
 from typing import Callable
 from typing import Optional
 
+import torch
+import torch.nn as nn
+
 
 class BlackScholesPDE(BasePDE):
 
@@ -16,7 +19,8 @@ class BlackScholesPDE(BasePDE):
             interest_rate: float,
             payoff: Callable,
             start_time: Optional[float] = None,
-            end_time: Optional[float] = None
+            end_time: Optional[float] = None,
+            device=None
     ):
         super().__init__(
             n_dimensions=n_dimensions,
@@ -32,3 +36,51 @@ class BlackScholesPDE(BasePDE):
         self.volatility = volatility
         self.interest_rate = interest_rate
         self.terminal_payoff = payoff
+
+        if device is not None:
+            self.device = device
+        else:
+            self.device = "cpu"
+
+    def zero_order_op(
+            self
+    ):
+        raise NotImplementedError
+
+    def first_order_op(
+            self,
+            y_hat: torch.Tensor,
+            x: torch.Tensor
+    ):
+        grads = torch.autograd.grad(
+            y_hat,
+            x,
+            grad_outputs=torch.ones(y_hat.shape).to(self.device),
+            retain_graph=True,
+            create_graph=True,
+            only_inputs=True)[0]
+
+        time_gradients, space_gradients = grads[:, 0].view(-1, 1), grads[:, 1].view(-1, 1)
+        return time_gradients + self.interest_rate * x * space_gradients
+
+    def second_order_op(
+            self,
+            space_gradients: torch.Tensor,
+            x: torch.Tensor,
+            monte_carlo: Optional[bool] = False
+    ):
+
+        if monte_carlo:
+            return torch.zeros(1)
+        else:
+            second_order_grads = torch.autograd.grad(
+                space_gradients,
+                x,
+                grad_outputs=torch.ones(space_gradients.shape).to(self.device),
+                create_graph=True,
+                only_inputs=True)[0][:, 1].view(-1, 1)
+
+            return 0.5 * torch.square(self.volatility * x) * second_order_grads
+
+
+
